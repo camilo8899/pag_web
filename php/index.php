fb45a4eb52f0ead228d67077605df80fb8a346da
camilo8899 <jcamilora@hotmail.com>
2018-10-22 20:51:41 -0500

    Agrego todo el proyecto

---
+++ php/index.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no,
+	 initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	 <link href='https://fonts.googleapis.com/css?family=Raleway:400,300' rel='stylesheet' type='text/css'>
+	 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+	 <link rel="stylesheet" href="css/estilos.css">
+	<title>Iniciar Sesión</title>
+</head>
+<body>
+	<div class="contenedor">
+		<h1 class="titulo">Iniciar Sesión</h1>
+		<hr class="border">
+
+		<form action="validar.php" method="POST" class="formulario" name="login">
+			<div class="form-group">
+				<i class="icono izquierda fa fa-user"></i><input type="text" name="mail" class="usuario" placeholder="Usuario">
+			</div>
+
+			<div class="form-group">
+				<i class="icono izquierda fa fa-lock"></i><input type="password" name="password" class="password_btn" placeholder="Contraseña">
+				<i class="submit-btn fa fa-arrow-right" onclick="login.submit()"></i>
+			</div>
+
+			<?php if(!empty($errores)): ?>
+				<div class="error">
+					<ul>
+						<?php echo $errores; ?>
+					</ul>
+				</div>
+			<?php endif; ?>
+		</form>
+
+		<p class="texto-registrate">
+			¿ Aun no tienes cuenta ?
+			<a href="registrate.php">Regístrate</a>
+		</p>
+	</div>
+</body>
+</html>
+
+	<!--<meta charset="utf-8">
+		<link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+		<link rel="stylesheet" href="bootstrap/css/bootstrap-responsive.css">
+		<link rel="stylesheet" type="text/css" href="estilos/estilos.css">
+	<title>Proyecto academias</title>
+</head>
+<body background="images/golf.jpg" style="background-attachment: fixed" >
+	<center><div class="tit"><h2 style="color: #0000FF; ">Inicio de sesión</h2>
+		<center><div class="Ingreso">
+
+	<table border="0" align="center" valign="middle">
+		<tr>
+		<td rowspan=2>
+		<form action="validar.php" method="post">
+
+		<table border="0">
+
+		<tr><td><label style="font-size: 14pt"><b>Correo: </b></label></td>
+			<td width=80> <input class="form-group has-success" style="border-radius:15px;" type="text" name="mail"></td></tr>
+		<tr><td><label style="font-size: 14pt"><b>Contraseña: </b></label></td>
+			<td witdh=80><input style="border-radius:15px;" type="password" name="pass"></td></tr>
+		<tr><td></td>
+			<td width=80 align=center><input class="btn btn-primary" type="submit" value="Aceptar"></td>
+			</tr></tr></table>
+		</form>
+<br>-->
+
+<!-- formulario registro -->
+
+<!--<form method="post" action="" >
+  <fieldset>
+    <legend  style="font-size: 18pt"><b>Registro</b></legend>
+    <div class="form-group">
+      <label style="font-size: 14pt"><b>Ingresa tu nombre</b></label>
+      <input type="text" name="realname" class="form-control" placeholder="Ingresa tu nombre" />
+    </div>
+    <div class="form-group">
+      <label style="font-size: 14pt; color: #FFFFFF;"><b>Ingresa tu email</b></label>
+      <input type="text" name="nick" class="form-control"  required placeholder="Ingresa mail"/>
+    </div>
+    <div class="form-group">
+      <label style="font-size: 14pt; color: #FFFFFF;"><b>Ingresa tu Password</b></label>
+      <input type="password" name="pass" class="form-control"  placeholder="Ingresa contraseña" />
+    </div>
+    <div class="form-group">
+      <label style="font-size: 14pt"><b>Repite tu contraseña</b></label>
+      <input type="password" name="rpass" class="form-control" required placeholder="repite contraseña" />
+    </div>
+      
+    </div>
+   
+    <input  class="btn btn-danger" type="submit" name="submit" value="Registrarse"/>
+
+  </fieldset>
+</form>
+</div>
+<!-php
+		//if(isset($_POST['submit'])){
+			//require("registro.php");
+		}
+	?>-->
+<!--Fin formulario registro -->
+
+		<!--</td>
+		</tr>
+		</table>
+		</div></center></div></center>
+
+	
+</body>
+</html>-->
